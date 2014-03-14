4da1b8061eeb1eb7fffc527a42328c3ead93ba8c
pieteradejong <pieter.a.dejong@gmail.com>
2014-03-14 15:22:28 -0700

    removed superfluous code and comments

--- src/com/codepath/tipcalculator/MainActivity.java
+++ src/com/codepath/tipcalculator/MainActivity.java
@@ -1,27 +1,17 @@
 package com.codepath.tipcalculator;
 
-
-
 import java.text.DecimalFormat;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
-//import android.support.v7.app.ActionBar;
-//import android.support.v4.app.Fragment;
-//import android.view.LayoutInflater;
-//import android.view.MenuItem;
-//import android.view.ViewGroup;
-//import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
 	private EditText etBillAmount;
 	private String strBillAmount;
 	private double dblBillAmount;
-//	private String strTipAmount;
     private EditText etTipAmount;
     private String strTipAmount;
 	
@@ -29,13 +19,6 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 	
 	public void calculateTip10Pct(View v) {
@@ -66,33 +49,4 @@ public class MainActivity extends ActionBarActivity {
 		etTipAmount = (EditText) findViewById(R.id.etTipAmount);
 		etTipAmount.setText(strTipAmount);
 	}
-//	public void calculateTip(View v) {
-//        String btnName = v.getResources().getResourceEntryName(v.getId());
-//		switch(btnName) {
-//	      case: btnName = "btn_tip_10pct";
-//	        tip_pct = 10;
-//	      case: btn_tip_15pct
-//	        tip_pct = 15;
-//	      case: btn_tip_20pct
-//	        tip_pct = 20;
-//	    }
-//	    textField.getText().toString()
-//		tip_amount = amount * 
-//	    // set text field to 
-//	    // calculate tip
-//	    // set tipAmount field value to this amount
-//	}	
-
-
-//	@Override
-//	public boolean onOptionsItemSelected(MenuItem item) {
-//		// Handle action bar item clicks here. The action bar will
-//		// automatically handle clicks on the Home/Up button, so long
-//		// as you specify a parent activity in AndroidManifest.xml.
-//		int id = item.getItemId();
-//		if (id == R.id.action_settings) {
-//			return true;
-//		}
-//		return super.onOptionsItemSelected(item);
-//	}
 }
